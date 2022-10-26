dab8af07af643f533844821c9660ae6c4f7fba43
Kevin <kevin@twjoin.com>
2022-10-26 16:40:00 +0800

    setup bottombottomNavigationBar

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -6,11 +6,13 @@ class AppIcon extends StatelessWidget {
   final Color bgColor;
   final Color iconColor;
   final double size;
+  final double? iconSize;
   const AppIcon({Key? key,
     required this.icon,
     this.iconColor = const Color(0xFF756d54),
     this.bgColor = const Color(0xFFfcf4e4),
-    this.size = 40
+    this.size = 40,
+    this.iconSize
   }) : super(key: key);
 
   @override
@@ -25,7 +27,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: AppLayout.getHeight(16),
+        size: iconSize ?? AppLayout.getHeight(16),
       ),
     );
   }
